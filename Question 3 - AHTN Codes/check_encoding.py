e23e024cdd884be6234e0b551c0c3ceb89dc6ee4
PyGeek <106702484+AmryAnsary@users.noreply.github.com>
2024-08-28 15:42:21 +0800

    Delete Question 3 - AHTN Codes directory

--- Question 3 - AHTN Codes/check_encoding.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Specify an alternative encoding
-ahtn_2017 = pd.read_csv(r'C:\Users\user\Downloads\AHTN_2017.csv', encoding='ISO-8859-1')
-ahtn_2022 = pd.read_csv(r'C:\Users\user\Downloads\AHTN_2022.csv', encoding='ISO-8859-1')
-
-print(ahtn_2017.head())
-print(ahtn_2022.head())
